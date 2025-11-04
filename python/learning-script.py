68a8a31f2281b31350392ef3d4d173740388a63a
hasankarim365 <hasankarim365@gmail.com>
2025-11-04 19:48:47 +0000

    Added definitions to Logical Operators programme

--- python/learning-script.py
+++ python/learning-script.py
@@ -114,7 +114,7 @@
 # temp = 30
 # is_sunny = True
 
-# if temp == 30 and not is_sunny:
+# if temp == 30 and not is_sunny:   # "and", "or" and "not" is a Logic operator
 #     print("It is hot and cloudy outside")
 # elif is_sunny:
 #     print("It is sunny")
