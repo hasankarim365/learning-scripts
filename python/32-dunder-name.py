beafc300032478afa694ff23b04aec34eac27cbd
hasankarim365 <hasankarim365@gmail.com>
2025-11-05 08:57:17 +0000

    Added 32-34 Chapters

---
+++ python/32-dunder-name.py
@@ -0,0 +1,6 @@
+def main():
+    print("lorem ipsum dolet sit amet")
+
+if __name__ == "__main__": # means execute this code if this .py script is being run directly, as opposed to being imported to another file
+    main()
+
