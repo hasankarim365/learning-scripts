ed8acc082e365a72439133e29c0498dcf3468238
PiThonics <hasankarim365@gmail.com>
2025-11-11 08:56:22 +0000

    Did upto Chapter 38

--- python/learning-script.py
+++ python/learning-script.py
@@ -710,3 +710,115 @@
 # mouse.sleep()
 # mouse.speak()
 
+
+#                   Number 36 (Multiple Inheritence)
+
+# class Prey():
+#     def __init__(self, name):
+#         self.name = name
+#     def flee(self):
+#         print(f"{self.name} is fleeing")
+
+# class Predator():
+#     def __init__(self, name):
+#         self.name = name
+#     def hunt(self):
+#         print(f"{self.name} is hunting")
+
+# class Rabbit(Prey):
+#     pass
+
+# class Hawk(Predator):
+#     pass
+
+# class Fish(Prey, Predator): # This is multiple inheritence, which is when a children class inherits from two parent classes
+#     pass
+
+# rabbit = Rabbit("Hops")
+# hawk = Hawk("Tony")
+# fish = Fish("Guppy")
+
+# fish.flee()
+# fish.hunt()
+
+# rabbit.flee()
+# hawk.hunt()
+
+
+#                   Number 37 (Multilevel Inheritence)
+
+class animal():
+    def __init__(self, name):
+        self.name = name
+    def eat(self):
+        print(f"{self.name} is eating")
+
+class Prey(animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey): # This is multilevel inheritence, which is where when a children class inherits from another children class
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Hops")
+hawk = Hawk("Tony")
+fish = Fish("Guppy")
+
+fish.eat()
+rabbit.eat()
+hawk.eat()
+
+
+#                   Number 38 (Super function)
+ 
+class shapes:
+    def __init__(self, colour, filled):
+        self.colour = colour
+        self.filled = filled
+
+class Circle(shapes):
+    def __init__(self, colour, filled, radius):
+        super().__init__(colour, filled)
+        self.radius = radius
+
+class Square(shapes):
+    def __init__(self, colour, filled, width):
+        super().__init__(colour, filled)
+        self.width = width
+
+class Triangle(shapes):
+    def __init__(self, colour, filled, width, height):
+        super().__init__(colour, filled) # This is the super function, which is a function used in a child class to call methods from a parent class, which is called a superclass.
+        #It allows you to extend the functionalit of the inherited methods
+        self.width = width
+        self.height = height
+
+circle = Circle("black", "filled", "5")
+square = Square(colour = "red", filled = "Not filled", width = "3")
+triangle = Triangle(colour = "blue", filled = "Not filled", width = "5", height= "6")
+
+print(circle.colour)
+print(circle.filled)
+print(circle.radius)
+
+print(square.colour)
+print(square.filled)
+print(square.width)
+
+print(triangle.colour)
+print(triangle.filled)
+print(triangle.width)
+print(triangle.height)
+
+
+#                   Number 39 ()
