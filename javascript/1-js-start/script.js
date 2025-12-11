5a3cd5736d4dca73aad0749345df021e34266fc4
PiThonics <hasankarim365@gmail.com>
2025-12-11 17:44:16 +0000

    Javascript start

---
+++ javascript/1-js-start/script.js
@@ -0,0 +1,7 @@
+console.log("Hello"); // This displays in the given message in the console
+console.log("Hello World");
+
+window.alert("This is an alert"); // This creates a alert wind on the web page
+
+document.getElementById("myH1").textcontent = "Hello"; // This calls an element of the html code using its ID, then adds text to it
+document.getElementById("myP").textcontent = "Lorem Ipsum Doler Sit Amet";
---
+++ javascript/variables/script.js
@@ -0,0 +1,13 @@
+// Variables are containers that stores a value
+// declaration establishes the variable in the code
+// assignment gives the variable a value
+
+let x; // This is an declaration
+x = 34; // This is an assignment
+console.log(x);
+
+let y = 35; // This line declares and establishes the variable
+console.log(y);
+
+console.log(`X = ${x}, Y = ${y}`); // This embeds the variables in the message
+console.log(typeof x); // This tells you what type of variable a variable is
